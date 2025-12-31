832ae08db5f22c3edc9297e0ccaa21a6b605c80d
Puneet Kumar Garg <puneetkumargarg@Puneets-MacBook-Air.local>
2025-12-31 15:20:07 +0530

    Add Delhi and Jaipur hospitals and traffic signals

---
+++ add-delhi-jaipur-data.sql
@@ -0,0 +1,35 @@
+-- Add traffic signals for Delhi
+INSERT INTO traffic_signals (signal_name, location_lat, location_lng, current_status, direction_ns, direction_sn, direction_ew, direction_we) VALUES
+('Connaught Place Signal', 28.6315, 77.2167, 'normal', 'Green', 'Green', 'Red', 'Red'),
+('India Gate Signal', 28.6129, 77.2295, 'normal', 'Green', 'Green', 'Red', 'Red'),
+('Red Fort Signal', 28.6562, 77.2410, 'normal', 'Green', 'Green', 'Red', 'Red'),
+('Karol Bagh Signal', 28.6519, 77.1909, 'normal', 'Green', 'Green', 'Red', 'Red'),
+('Lajpat Nagar Signal', 28.5677, 77.2436, 'normal', 'Green', 'Green', 'Red', 'Red'),
+('Saket Signal', 28.5245, 77.2066, 'normal', 'Green', 'Green', 'Red', 'Red'),
+('Dwarka Signal', 28.5921, 77.0460, 'normal', 'Green', 'Green', 'Red', 'Red'),
+('Rohini Signal', 28.7041, 77.1025, 'normal', 'Green', 'Green', 'Red', 'Red'),
+('Janakpuri Signal', 28.6219, 77.0814, 'normal', 'Green', 'Green', 'Red', 'Red'),
+('Vasant Kunj Signal', 28.5245, 77.1581, 'normal', 'Green', 'Green', 'Red', 'Red'),
+('Nehru Place Signal', 28.5495, 77.2500, 'normal', 'Green', 'Green', 'Red', 'Red'),
+('Rajouri Garden Signal', 28.6519, 77.1200, 'normal', 'Green', 'Green', 'Red', 'Red'),
+('Pitampura Signal', 28.6942, 77.1314, 'normal', 'Green', 'Green', 'Red', 'Red'),
+('Mayur Vihar Signal', 28.6139, 77.2973, 'normal', 'Green', 'Green', 'Red', 'Red'),
+('Preet Vihar Signal', 28.6239, 77.2873, 'normal', 'Green', 'Green', 'Red', 'Red');
+
+-- Add traffic signals for Jaipur
+INSERT INTO traffic_signals (signal_name, location_lat, location_lng, current_status, direction_ns, direction_sn, direction_ew, direction_we) VALUES
+('MI Road Signal', 26.9124, 75.7873, 'normal', 'Green', 'Green', 'Red', 'Red'),
+('Statue Circle Signal', 26.9224, 75.7973, 'normal', 'Green', 'Green', 'Red', 'Red'),
+('Ajmeri Gate Signal', 26.9024, 75.7773, 'normal', 'Green', 'Green', 'Red', 'Red'),
+('Sanganeri Gate Signal', 26.9000, 75.8000, 'normal', 'Green', 'Green', 'Red', 'Red'),
+('Bani Park Signal', 26.9300, 75.7800, 'normal', 'Green', 'Green', 'Red', 'Red'),
+('Malviya Nagar Signal', 26.8467, 75.8056, 'normal', 'Green', 'Green', 'Red', 'Red'),
+('Vaishali Nagar Signal', 26.9500, 75.7300, 'normal', 'Green', 'Green', 'Red', 'Red'),
+('Mansarovar Signal', 26.8500, 75.7500, 'normal', 'Green', 'Green', 'Red', 'Red'),
+('Jagatpura Signal', 26.8200, 75.8500, 'normal', 'Green', 'Green', 'Red', 'Red'),
+('Sodala Signal', 26.9200, 75.8200, 'normal', 'Green', 'Green', 'Red', 'Red'),
+('Tonk Road Signal', 26.8800, 75.8100, 'normal', 'Green', 'Green', 'Red', 'Red'),
+('JLN Marg Signal', 26.9100, 75.7900, 'normal', 'Green', 'Green', 'Red', 'Red'),
+('C Scheme Signal', 26.9000, 75.8000, 'normal', 'Green', 'Green', 'Red', 'Red'),
+('Vidyadhar Nagar Signal', 26.9800, 75.7600, 'normal', 'Green', 'Green', 'Red', 'Red'),
+('Sikar Road Signal', 26.9600, 75.7400, 'normal', 'Green', 'Green', 'Red', 'Red');
--- src/hooks/useHospitals.tsx
+++ src/hooks/useHospitals.tsx
@@ -12,18 +12,32 @@ export interface Hospital {
 
 // Default hospital locations with all details (using generated UUIDs)
 const DEFAULT_HOSPITALS: Hospital[] = [
+  // Chandigarh Hospitals
   { id: '00000000-0000-0000-0000-000000000001', organization_name: 'PGIMER Chandigarh', email: '', full_name: null, location_lat: 30.7649, location_lng: 76.7757 },
   { id: '00000000-0000-0000-0000-000000000002', organization_name: 'GMCH Sector 32', email: '', full_name: null, location_lat: 30.7422, location_lng: 76.7676 },
   { id: '00000000-0000-0000-0000-000000000003', organization_name: 'Fortis Hospital Mohali', email: '', full_name: null, location_lat: 30.7133, location_lng: 76.6912 },
   { id: '00000000-0000-0000-0000-000000000004', organization_name: 'Max Super Speciality', email: '', full_name: null, location_lat: 30.7046, location_lng: 76.7179 },
   { id: '00000000-0000-0000-0000-000000000005', organization_name: 'Ivy Hospital', email: '', full_name: null, location_lat: 30.7081, location_lng: 76.7104 },
   { id: '00000000-0000-0000-0000-000000000006', organization_name: 'Alchemist Hospital', email: '', full_name: null, location_lat: 30.7254, location_lng: 76.7408 },
-  { id: '00000000-0000-0000-0000-000000000007', organization_name: 'Indus Hospital', email: '', full_name: null, location_lat: 30.7055, location_lng: 76.7245 },
-  { id: '00000000-0000-0000-0000-000000000008', organization_name: 'Healing Hospital', email: '', full_name: null, location_lat: 30.7185, location_lng: 76.7325 },
-  { id: '00000000-0000-0000-0000-000000000009', organization_name: 'SPS Apollo Hospital', email: '', full_name: null, location_lat: 30.6912, location_lng: 76.7489 },
-  { id: '00000000-0000-0000-0000-000000000010', organization_name: 'Silver Oaks Hospital', email: '', full_name: null, location_lat: 30.7389, location_lng: 76.7812 },
-  { id: '00000000-0000-0000-0000-000000000011', organization_name: 'Mukat Hospital', email: '', full_name: null, location_lat: 30.7512, location_lng: 76.7645 },
-  { id: '00000000-0000-0000-0000-000000000012', organization_name: 'Grecian Hospital', email: '', full_name: null, location_lat: 30.7095, location_lng: 76.6878 },
+  
+  // Delhi Hospitals
+  { id: '00000000-0000-0000-0000-000000000020', organization_name: 'AIIMS Delhi', email: '', full_name: null, location_lat: 28.5672, location_lng: 77.2100 },
+  { id: '00000000-0000-0000-0000-000000000021', organization_name: 'Safdarjung Hospital', email: '', full_name: null, location_lat: 28.5738, location_lng: 77.2088 },
+  { id: '00000000-0000-0000-0000-000000000022', organization_name: 'Ram Manohar Lohia Hospital', email: '', full_name: null, location_lat: 28.6358, location_lng: 77.2244 },
+  { id: '00000000-0000-0000-0000-000000000023', organization_name: 'Sir Ganga Ram Hospital', email: '', full_name: null, location_lat: 28.6454, location_lng: 77.1907 },
+  { id: '00000000-0000-0000-0000-000000000024', organization_name: 'Max Saket Delhi', email: '', full_name: null, location_lat: 28.5245, location_lng: 77.2066 },
+  { id: '00000000-0000-0000-0000-000000000025', organization_name: 'Fortis Shalimar Bagh', email: '', full_name: null, location_lat: 28.7196, location_lng: 77.1647 },
+  { id: '00000000-0000-0000-0000-000000000026', organization_name: 'Apollo Delhi', email: '', full_name: null, location_lat: 28.5672, location_lng: 77.2773 },
+  { id: '00000000-0000-0000-0000-000000000027', organization_name: 'BLK Super Speciality', email: '', full_name: null, location_lat: 28.6507, location_lng: 77.2334 },
+  
+  // Jaipur Hospitals
+  { id: '00000000-0000-0000-0000-000000000030', organization_name: 'SMS Hospital Jaipur', email: '', full_name: null, location_lat: 26.9124, location_lng: 75.7873 },
+  { id: '00000000-0000-0000-0000-000000000031', organization_name: 'Fortis Escorts Jaipur', email: '', full_name: null, location_lat: 26.8467, location_lng: 75.8056 },
+  { id: '00000000-0000-0000-0000-000000000032', organization_name: 'Narayana Hospital Jaipur', email: '', full_name: null, location_lat: 26.8467, location_lng: 75.8156 },
+  { id: '00000000-0000-0000-0000-000000000033', organization_name: 'Manipal Hospital Jaipur', email: '', full_name: null, location_lat: 26.9800, location_lng: 75.7600 },
+  { id: '00000000-0000-0000-0000-000000000034', organization_name: 'Max Hospital Jaipur', email: '', full_name: null, location_lat: 26.9000, location_lng: 75.8000 },
+  { id: '00000000-0000-0000-0000-000000000035', organization_name: 'CK Birla Hospital', email: '', full_name: null, location_lat: 26.9200, location_lng: 75.8200 },
+  { id: '00000000-0000-0000-0000-000000000036', organization_name: 'Eternal Hospital Jaipur', email: '', full_name: null, location_lat: 26.8200, location_lng: 75.8500 },
 ];
 
 // Hospital location lookup for DB hospitals
